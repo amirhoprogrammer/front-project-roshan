4c8faa66cb8429165a148192262d6d555a19d473
amihoprogrammer <amirhanegareshi@gmail.com>
2025-07-16 20:44:52 +0330

    add router of Tab and make Tabs

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import ChangeTheVoice from "./pages/ChangeTheVoice.jsx";
 import Guest from "./feathers/Guest";
 import Archive from "./pages/Archive.jsx";
+import Tabs from "./feathers/Tabs";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/changethevoice" element={<ChangeTheVoice />} />
       <Route path="/archive" element={<Archive />} />
       <Route path="/guest" element={<Guest />} />
+      <Route path="/tabs" element={<Tabs />} />
     </Routes>
   );
 }
