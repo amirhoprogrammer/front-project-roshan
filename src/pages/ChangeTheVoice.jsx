f3f165fbbcd3a28bbbdefa82a913686570b0b5c2
amihoprogrammer <amirhanegareshi@gmail.com>
2025-07-16 20:47:52 +0330

    add Tabs in changeThevoice

--- src/pages/ChangeTheVoice.jsx
+++ src/pages/ChangeTheVoice.jsx
@@ -1,33 +1,10 @@
 import { useState, React } from "react";
 import Guest from "../feathers/Guest";
+import Tabs from "../feathers/Tabs";
 //import { Tabs, Tab, Box } from "@mui/material";
 //import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "../App.css";
 function ChangeTheVoice() {
-  const [activeTab, setActiveTab] = useState(0);
-  const tabs = [
-    { label: "ضبط صدا", content: "محتوای تب اول" },
-    { label: "بارگزاری فایل", content: "محتوای تب دوم" },
-    { label: "لینک", content: "محتوای تب سوم" },
-  ];
-  const Tab = ({ label, isActive, onClick }) => (
-    <button
-      className={`px-4 py-2 font-medium ${
-        isActive ? "text-white active rounded-t-2xl" : "deactive rounded-2xl"
-      }`}
-      onClick={onClick}
-    >
-      {label}
-    </button>
-  );
-  {
-    /*style = {`${isActive ? { borderColor: "" } : { borderColor: "white" }}`
-     */
-  }
-  const TabContent = ({ isActive, children }) => (
-    <div className={`p-4 ${isActive ? "block" : "hidden"}`}>{children}</div>
-  );
-
   return (
     <div className="mt-8 mb-4 flex flex-col">
       <Guest />
@@ -45,23 +22,7 @@ function ChangeTheVoice() {
         می‌تواند متن صحبت‌ها را بنویسد
       </p>
       <div className="p-0 max-w-md mx-auto mt-4">
-        <div className="flex" style={{ direction: "rtl" }}>
-          {tabs.map((tab, index) => (
-            <Tab
-              key={index}
-              label={tab.label}
-              isActive={activeTab === index}
-              onClick={() => setActiveTab(index)}
-            />
-          ))}
-        </div>
-        <div className="border-2 rounded-lg rounded-tr-none" style={{ borderColor: "#00B3A1" }}>
-          {tabs.map((tab, index) => (
-            <TabContent key={index} isActive={activeTab === index}>
-              {tab.content}
-            </TabContent>
-          ))}
-        </div>
+        <Tabs />
       </div>
       <div className="flex mx-84 my-5 items-start w-48">
         <select
