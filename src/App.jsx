d01134013f11a3377638a84c850e3cc0d586d9ad
amihoprogrammer <amirhanegareshi@gmail.com>
2025-07-19 21:55:51 +0330

    add route of BasicText and TimeText

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Archive from "./pages/Archive.jsx";
 import Tabs from "./feathers/Tabs";
 import Language from "./feathers/Language";
 import UploadHome from "./feathersOfUpload/UploadHome";
+import TimeText from "./feathersOfUpload/TimeText";
+import BasicText from "./feathersOfUpload/BasicText";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
@@ -19,6 +21,8 @@ function App() {
       <Route path="/tabs" element={<Tabs />} />
       <Route path="/language" element={<Language />} />
       <Route path="/uploadhome" element={<UploadHome />} />
+      <Route path="/timetext" element={<TimeText />} />
+      <Route path="/basictext" element={<BasicText />} />
     </Routes>
   );
 }
